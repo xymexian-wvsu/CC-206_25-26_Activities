a69b15bc4a41857231bc4f17bfbdb2beb8d071d9
Pkxen <xymex.miralles@wvsu.edu.ph>
2026-05-10 15:51:38 +0800

    UNIT-6-ASSIGNMENT-SUBMSSION-MIRALLES

---
+++ CS 2A/XYMEX_MIRALLES/XYMEX-UNIT_6_ACTIVITY/first_app/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<audioplayers_darwin/AudioplayersDarwinPlugin.h>)
+#import <audioplayers_darwin/AudioplayersDarwinPlugin.h>
+#else
+@import audioplayers_darwin;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AudioplayersDarwinPlugin registerWithRegistrar:[registry registrarForPlugin:@"AudioplayersDarwinPlugin"]];
+}
+
+@end
